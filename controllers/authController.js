0b9357c3bc5e4b42c8a4c348b8b22eb85f9e1903
PATEL SANDESH <patelsandesh1@gmail.com>
2025-03-18 04:26:05 +0530

    Fixed bcrypt error by using bcryptjs

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,6 @@
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const Account = require("../models/Account");
 
 const registerUser = async (req, res) => {
     try {
